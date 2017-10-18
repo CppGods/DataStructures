cf197975caf8febea03e49dabdbccf18c9c3fbdc
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-18 16:28:22 +0300

    Update stack.hpp

--- include/stack/stack.hpp
+++ include/stack/stack.hpp
@@ -2,27 +2,27 @@ template <class Ty>
 class stack {
 
 public:
-
-	//methods
-
-/*
-stack::empty	Проверяет, является ли stack пустым.
-stack::pop		Удаляет элемент из верхней части stack.
-stack::push	    Добавляет элемент в верхнюю часть stack.
-stack::size	    Возвращает количество элементов в контейнере stack.
-stack::Top	    Возвращает ссылку на элемент в верхней части stack.
-*/
-	//...
-
-	//operators
-
-	//...
-
+bool empty() const noexcept;
+stack();
+~stack()  noexcept;
 private:
-
-	//members
 
 	Ty* array_;
 	size_t size_array_;
 	size_t count_;
 };
+template <class Ty>
+bool stack<Ty>:: empty() const noexcept
+{
+	return count_==0;
+}
+template <class Ty>
+stack<Ty>::stack(): count_{0}, array_size_{0}, array_{nullptr}
+{
+}
+
+template <class Ty>
+stack<Ty>::~stack() noexcept
+{
+	delete[] array_;
+}

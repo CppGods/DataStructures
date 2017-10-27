9fb50434881a546add3e75f6518823b234a67617
SVolkoff <sabina.volkoff@gmail.com>
2017-10-27 21:55:28 +0300

    add data(), max_size(); remove iterators

--- include/array/array.hpp
+++ include/array/array.hpp
@@ -1,84 +1,101 @@
 #include <algorithm>
 #include <stdexcept>
 
-
-template<typename T, size_t N>
+template<typename T, std::size_t N>
 class array
 {
 public:
-	
-	
-	typedef typename std::add_pointer<T>::type iterator;
-	
-	size_t size() const
-	{ 
-		return N;
-	}
-
-	bool empty() const 
-	{
-		return !N;
-	}
-
-	T& operator [](size_t idx) 
-	{ 
-		if (idx >= size())
-			throw std::out_of_range("out_of_range");
-		return array_[idx];
-	}
-
-	T& at(size_t idx)
-	{
-		if (idx >= size())
-			throw std::out_of_range("out_of_range");
-		return array_[idx];
-	}
-
-	T& front()
-	{
-		return *begin();
-	}
-	
-	T& back()
-	{
-		return N ? *(end() - 1) : *end();
-	}
-
-	iterator begin() 
-	{
-		return iterator(array_); 
-	}
-
-	iterator end() 
-	{
-		return iterator(array_ + N);
-	}
-
-
-	void swap(array& other)
-	{
-		std::swap_ranges(begin(), end(), other.begin());
-	}
-
-	void fill(T * val)
-	{
-		for (int i = 0; i < N; i++)
-		{
-			*val = array_[i];
-			val++;
-		}
-	}
-	array<T, N> & operator=(const array<T, N> & arr)
-	{
-		if (this != arr) 
-		{
-			array<T, N> temp(arr);
-			temp.swap(*this);
-		}
-		return *this;
-	}
-	
+	std::size_t size() const noexcept;
+	bool empty() const  noexcept;
+	T& operator [](std::size_t idx);
+	T& at(std::size_t idx);
+	T& front();
+	T& back();
+	T* data();
+	void swap(array& other);
+	void fill(T * val);
+	array<T, N> & operator=(const array<T, N> & arr);
+	std::size_t max_size() const noexcept;
 private:
-	T array_[N ? N : 1];
-
+	T array_[N];
 };
+
+template<typename T, std::size_t N>
+std::size_t array<T, N>::size() const noexcept
+{
+	return N;
+}
+
+template<typename T, std::size_t N>
+bool array<T, N>::empty() const  noexcept
+{
+	return !N;
+}
+
+template<typename T, std::size_t N>
+T& array<T, N>::operator [](std::size_t idx)
+{
+	if (idx >= size())
+		throw std::out_of_range("out_of_range");
+	return array_[idx];
+}
+
+template<typename T, std::size_t N>
+T& array<T, N>::at(std::size_t idx)
+{
+	if (idx >= size())
+		throw std::out_of_range("out_of_range");
+	return array_[idx];
+}
+
+template<typename T, std::size_t N>
+T& array<T, N>::front()
+{
+	return array_[0];
+}
+
+template<typename T, std::size_t N>
+T& array<T, N>::back()
+{
+	return array_[N - 1];
+}
+
+template<typename T, std::size_t N>
+T* array<T, N>::data()
+{
+	return &array_[0];
+}
+
+template<typename T, std::size_t N>
+void array<T, N>::swap(array& other)
+{
+	std::swap_ranges(data(), data() + N, other.data());
+}
+
+template<typename T, std::size_t N>
+void array<T, N>::fill(T * val)
+{
+	for (int i = 0; i < N; i++)
+	{
+		*val = array_[i];
+		val++;
+	}
+}
+
+template<typename T, std::size_t N>
+array<T, N> & array<T, N>::operator=(const array<T, N> & arr)
+{
+	if (this != &arr)
+	{
+		array<T, N> temp(arr);
+		temp.swap(*this);
+
+	}
+	return *this;
+}
+ 
+template<typename T, std::size_t N>
+std::size_t array<T, N>::max_size() const noexcept
+{
+	return N;
+}

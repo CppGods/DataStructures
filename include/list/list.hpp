355ecfa476f633666ff6a776a3356e58be6b454f
NeverMore27 <uragan.uragan.59@mail.ru>
2017-11-05 23:49:33 +0300

    Update list.hpp

--- include/list/list.hpp
+++ include/list/list.hpp
@@ -1,23 +1,40 @@
+#include <iostream>
+
 template <class Ty>
-class list {
-
-public:
-
-  list();
-  
-  list(const list& other);
-  
-  ~list();
-  
-  //...
- 
-  list& operator=(const list& other);
-  
-  //..
-
+class list 
+{
 private:
-  
+   node* head;
+   size_t count;
+   struct node
+   {
+       Ty data;
+       node* next;
+   }
+public:
+  list();
+  list(const list& other);
+  ~list();
+  list& operator=(const list& other);
+private:
   void swap(list& other);
-  
-  //...
 };
+template <class Ty>
+list<Ty>::list(): count{0}, head{nullptr}{};
+template <class Ty>
+list<Ty>::~list()
+{
+    node *cur_ = head;
+		node *deleted_ = head;
+		while (deleted_!=nullptr)
+		{
+			  cur_=cur_->next;
+			  delete deleted_;
+			  deleted_=current;
+		}
+}
+template <class Ty>
+list<Ty>::list(const list& other)
+{
+  
+}

13278cffe817bcaa45351633a2df34406514d058
NeverMore27 <uragan.uragan.59@mail.ru>
2017-11-06 00:39:52 +0300

    Update list.hpp

--- include/list/list.hpp
+++ include/list/list.hpp
@@ -4,24 +4,29 @@ template <class Ty>
 class list 
 {
 private:
-   node* head;
+   node *head, *tail;
    size_t count;
    struct node
    {
        Ty data;
        node* next;
+       node* prev;
+       node() : next(nullptr) {};
+       node(const Ty& t) : data{t}, next{nullptr}, prev(nullptr){};
    }
 public:
   list();
   list(const list& other);
   ~list();
   list& operator=(const list& other);
+  bool empty();
+  size_t size();
 private:
   void swap(list& other);
   void clear();
 };
 	template <class Ty>
-	list<Ty>::list(): count{0}, head{nullptr}{};
+	list<Ty>::list(): count{0}, head{nullptr}, tail{nullptr}{};
 	template <class Ty>
 	list<Ty>::~list()
 	{
@@ -30,16 +35,15 @@ private:
 	template <class Ty>
 	void list<Ty>::clear()
 	{
-		node *cur_ = head;
-			node *deleted_ = head;
-			while (deleted_!=nullptr)
-			{
-				  cur_=cur_->next;
-				  delete deleted_;
-				  deleted_=cur_;
-			}
+		while (head) 
+     		{
+        		 tail=head->next; 
+        		 delete head;
+         		 head=tail; 
+     		}
 		count=0;
 		head=nullptr;
+		tail=nullptr;
 	}
 	template <class Ty>
 	list<Ty>::list(const list& other)
@@ -48,7 +52,17 @@ private:
 		while (other.head!=nullptr)
 		{
 			add(oter.head);
+			other.head=other.head->next;
 		}
-		
 	}
-	
+	template< typename Ty >
+	bool list<Ty>::empty() 
+	{
+   		 return (count==0);
+   	}
+	template< typename Ty >
+	size_t list<Ty>::size() 
+	{
+   		 return count;
+   	}
+}

3a45e56ef425c83fa035daf590495380412f1691
SVolkoff <sabina.volkoff@gmail.com>
2017-11-19 21:19:19 +0300

    added deque general methods

--- include/deque/deque.hpp
+++ include/deque/deque.hpp
@@ -1,1 +1,146 @@
+#include"include\list\list.hpp"
 
+template <class Type>
+class Deque 
+{
+		list<Type> _data;
+	public:
+		Deque();
+		~Deque();
+		Deque(const Type& value);
+		size_t size();
+		void swap(Deque& other);
+		Deque& operator=(const Deque& other);
+		Type& operator[](size_t Index)const;
+		Type&	at(size_t Index)const;
+		Type& back() const;
+		Type& front() const;
+		bool empty() const;
+		void insert(size_t Index, const Type &val);
+		void clear();
+		void push_back(const Type &val);
+		void push_front(const Type &val);
+		void pop_back();
+		void pop_front();
+		void erase(size_t Index);
+};
+
+template<class Type>
+Deque<Type>::Deque()
+{
+	
+}
+
+template<class Type>
+size_t Deque<Type>::size()
+{
+	return _data.count();
+}
+
+template <class Type>
+Deque<Type>& Deque<Type>:: operator=(const Deque<Type>& other)
+{
+	_data = other._data;
+}
+template<class Type>
+Type & Deque<Type>::operator[](size_t Index) const
+{
+	Node<Type> *before = _data->head;
+	while (Index-- > 0)
+		before = before->next;
+	return before->data;
+}
+template<class Type>
+Type & Deque<Type>::at(size_t Index) const
+{
+	Node<Type> *before = _data.head;
+	while (Index-- > 0)
+		before = before->next;
+	return before->data;
+}
+template<class Type>
+Type & Deque<Type>::back() const
+{
+	return _data.back();
+}
+template<class Type>
+Type & Deque<Type>::front() const
+{
+	return _data.front();
+}
+template<class Type>
+bool Deque<Type>::empty() const
+{
+	return _data.empty();
+}
+template<class Type>
+void Deque<Type>::insert(size_t Index, const Type & val)
+{
+	_data.insert();
+}
+template<class Type>
+void Deque<Type>::clear()
+{
+	_data.clear();
+}
+template<class Type>
+void Deque<Type>::push_back(const Type & val)
+{
+	_data.push_back(val);
+}
+template<class Type>
+void Deque<Type>::push_front(const Type & val)
+{
+	_data.push_front(val);
+}
+template<class Type>
+void Deque<Type>::pop_back()
+{
+	_data.pop_back();
+}
+template<class Type>
+void Deque<Type>::pop_front()
+{
+	_data.pop_front();
+}
+template<class Type>
+void Deque<Type>::erase(size_t Index)
+{
+	if (Index == 0)
+	{
+		_data.pop_front();
+	}
+	else
+		if (Index > count_)
+		{
+			_data.pop_back();
+		}
+		else
+		{
+			Node<Type> *before = _data.head;
+			while (Index-- > 0)
+				before = before->next;
+			Node<Type> *tmp = new Node<Type>;
+			before->prev->next = before->next;
+			before->next->prev = before->prev;
+			delete before;
+		}
+}
+
+template<class Type>
+void Deque<Type>::swap(Deque & other)
+{
+	_data.swap(other._data);
+}
+
+template<class Type>
+Deque<Type>::~Deque()
+{
+	_data.clear();
+}
+
+template<class Type>
+Deque<Type>::Deque(const Type & value)
+{
+	_data = other._data;
+}

97e48db7a3ae07582c2a1aa2f7ad9c760f0f2e46
NeverMore27 <uragan.uragan.59@mail.ru>
2017-11-19 20:16:53 +0300

    add queue metods

--- include/queue/queue.cpp
+++ include/queue/queue.cpp
@@ -1,1 +1,61 @@
 
+
+template <class Ty>
+class Queue
+{
+	forward_list<Ty> _items;
+public:
+	Queue();
+	~Queue();
+	void Enqueue(const Ty& value);
+	Ty Dequeue();
+	size_t count();
+	void show();
+	void swap(Queue& other);
+	Queue& operator=(const Queue& other);
+};
+
+template <class Ty>
+Queue<Ty>& Queue<Ty>:: operator=(const Queue<Ty>& other)
+{
+	_items = other._items;
+}
+
+template <class Ty>
+Queue<Ty>::Queue() {}
+
+template <class Ty>
+Queue<Ty>::~Queue()
+{
+	_items.clear();
+}
+
+template <class Ty>
+void Queue<Ty>:: Enqueue(const Ty& value)
+	{
+		_items.emplace_front(value);
+	}
+
+template <class Ty>
+Ty Queue<Ty>::Dequeue()
+	{
+		return _items.pop_back();
+	}
+
+template <class Ty> 
+size_t Queue<Ty>::count()
+{
+	return _items.count();
+}
+
+template <class Ty>
+void Queue<Ty>::show()
+{
+	_items.Show();
+}
+
+template <class Ty>
+void Queue<Ty>::swap(Queue& other)
+{
+	_items.swap(other._items);
+}

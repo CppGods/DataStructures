1b1bb454d4f2007fc9ba35da5930f87f8e2272bd
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-17 16:06:28 +0300

    Update rb-tree.cpp

--- tests/rb-tree.cpp
+++ tests/rb-tree.cpp
@@ -2,51 +2,51 @@
 #include <catch.hpp>
 
 
-SCENARIO("null")
+SCENARIO("RB null")
 {
 	RBtree<int, int> a;
 	REQUIRE(a.isEmpty());
 }
-SCENARIO("add")
+SCENARIO("RB add")
 {
 	RBtree <int, int> a;
 	a.Insert(5, 6);
 	REQUIRE(!a.isEmpty());
 }
-SCENARIO("BST delete non inserted element", "[delete]")
+SCENARIO("RB delete non inserted element", "[delete]")
 {
 	RBtree<int, int> tree = { {8, 7} };
 	tree.Remove(4);
 	REQUIRE(!tree.isEmpty());
 }
-SCENARIO("BST delete root with one element", "[delete]")
+SCENARIO("RB delete root with one element", "[delete]")
 {
 	RBtree<int, int> tree = { {8,8} };
 	tree.Remove(8);
 	REQUIRE(tree.isEmpty());
 }
-SCENARIO("BST delete root without child", "[delete]")
+SCENARIO("RB delete root without child", "[delete]")
 {
 	RBtree<int, int> tree = { {2,8}, {5, 4}, {5, 3} };
 	tree.Remove(8);
 	RBtree<int, int> tr ={ {5, 4}, {5, 3}};
 	REQUIRE(tree ==tr);
 }
-SCENARIO("BST delete root with two children", "[delete]")
+SCENARIO("RB delete root with two children", "[delete]")
 {
 	RBtree<int, int> tree = { {1, 8}, {18, 4}, {22, 3}, {2,10}, {3, 9}, {12,13}, {7,11}, {1,12} };
 	tree.Remove(4);
 	RBtree<int, int>tr={ {3, 9}, {1, 8},  {7,11}, {22, 3},{2,10}, {12,13}, {1,12} };
 	REQUIRE(tree ==tr );
 }
-SCENARIO("BST delete non root without children", "[delete]")
+SCENARIO("RB delete non root without children", "[delete]")
 {
 	RBtree<int, int> tree = { {1, 8}, {18, 4}, {22, 3}, {2,10}, {3, 9}, {12,13}, {7,11}, {1,12} };
 	tree.Remove(3);
 	RBtree<int, int>tr={{3, 9}, {18, 4}, {7,11}, {2,10}, {1,12}, {12,13}, {1, 8}} ;
 	REQUIRE(tree == tr);
 }
-SCENARIO("BST delete non root with one child", "[delete]")
+SCENARIO("RB delete non root with one child", "[delete]")
 {
 	RBtree<int, int> tree = { {1, 8}, {18, 4}, {22, 3}, {2,10}, {3, 9}, {12,13}, {7,11}, {1,12} };
 	tree.Remove(12);
